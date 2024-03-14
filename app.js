8cdb2d43a39d96ebef1c0273e76b40302d7a28ce
Arish Arsalan <159803393+Arish-Arsalan@users.noreply.github.com>
2024-03-14 20:32:20 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+let members = ['bisma', 'muntaha', 'mahnoor', 'urooj'];
+for (let i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
---
+++ app.ts
@@ -0,0 +1,4 @@
+let members: string[] =['bisma','muntaha','mahnoor','urooj'];
+for(let i=0; i<members.length; i++){
+    console.log(members[i]);
+}
